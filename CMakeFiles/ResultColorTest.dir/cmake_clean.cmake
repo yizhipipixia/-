0acf0418663ccb54cb1dcf2b3e07bf3c6fda8f73
yizhipipixia <65219051+yizhipipixia@users.noreply.github.com>
2020-09-23 01:51:55 +0800

    cs

---
+++ CMakeFiles/ResultColorTest.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/ResultColorTest.dir/ResultColorTest.cpp.o"
+  "ResultColorTest.pdb"
+  "ResultColorTest"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/ResultColorTest.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
